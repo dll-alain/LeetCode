eb2ca594eef8062df96d6ac4ba35e1634655e369
Alain <18817636063@163.com>
2022-05-12 21:18:08 +0800

    20220512

---
+++ src/indi/alain/Leetcode/onetoten/SortSquares.java
@@ -0,0 +1,32 @@
+package indi.alain.Leetcode.onetoten;
+
+import java.util.Arrays;
+
+/**
+ * @author d
+ */
+public class SortSquares {
+
+    public static int[] sortSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int k = nums.length - 1;
+        int[] temp = new int[nums.length];
+        while (left <= right) {
+            if (nums[right] * nums[right] > nums[left] * nums[left]) {
+                temp[k--] = nums[right] * nums[right];
+                right--;
+            } else {
+                temp[k--] = nums[left] * nums[left];
+                left++;
+            }
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-100, -3, 0, 1, 2, 3, 4};
+        System.out.println(Arrays.toString(sortSquares(nums)));
+    }
+
+}

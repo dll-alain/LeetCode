c6a3dc01051c1624583271bf1589cd4013aad614
Alain <18817636063@163.com>
2022-05-18 10:53:50 +0800

    20220518

---
+++ src/indi/alain/Leetcode/LinkList/RemoveNthFromEnd.java
@@ -0,0 +1,59 @@
+package indi.alain.Leetcode.LinkList;
+
+/**
+ * @author d
+ */
+public class RemoveNthFromEnd {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode removeNode(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int count = 0;
+        ListNode pre = dummy;
+        while (pre.next != null) {
+            pre = pre.next;
+            count++;
+        }
+        pre = dummy;
+        int i = count - n;
+        while (i > 0) {
+            pre = pre.next;
+            i--;
+        }
+        pre.next = pre.next.next;
+        return dummy.next;
+    }
+
+    /**
+     * 双指针法
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = null;
+        ListNode slowIndex = dummy;
+        ListNode fastIndex = dummy;
+        while (n-- > 0) {
+            fastIndex = fastIndex.next;
+        }
+        while (fastIndex != null) {
+            pre = slowIndex;
+            slowIndex = slowIndex.next;
+            fastIndex = fastIndex.next;
+        }
+        pre.next = slowIndex.next;
+        slowIndex = null;
+        return dummy.next;
+    }
+}

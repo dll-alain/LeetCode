d32a864f3265f909e5976db548bddc8826fa968a
Alain <18817636063@163.com>
2022-05-19 10:25:51 +0800

    20220519

---
+++ src/indi/alain/Leetcode/LinkList/Intersection.java
@@ -0,0 +1,51 @@
+package indi.alain.Leetcode.LinkList;
+import indi.alain.Leetcode.LinkList.RemoveNthFromEnd.ListNode;
+
+/**
+ * @author d
+ */
+public class Intersection {
+    public RemoveNthFromEnd.ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int sizeA = getSize(headA);
+        int sizeB = getSize(headB);
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        int gap = 0;
+        if (sizeA > sizeB) {
+            gap = sizeA - sizeB;
+            for (int i = 0; i < gap; i++) {
+                tempA = tempA.next;
+            }
+        }else {
+            gap = sizeB - sizeA;
+            for (int i = 0; i < gap; i++) {
+                tempB = tempB.next;
+            }
+        }
+
+        ListNode intersect = null;
+        while (tempA != null) {
+            if ( intersect == null && tempA == tempB) {
+                intersect = tempA;
+            }
+            if (tempA != tempB) {
+                intersect = null;
+            }
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+        return intersect;
+
+
+    }
+
+    public int getSize(ListNode head) {
+        ListNode temp = head;
+        int count = 0;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+}

af1ad3612bae461fe39698b909bd600124c6b82b
Alain <18817636063@163.com>
2022-09-04 12:26:20 +0800

    0904

---
+++ src/indi/alain/Leetcode/binarytree/MaxDepth.java
@@ -0,0 +1,50 @@
+package indi.alain.Leetcode.binarytree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * e二叉树最大深度
+ * @author d
+ */
+public class MaxDepth {
+
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int depth = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            depth++;
+            while (len-- > 0) {
+                Node temp = queue.poll();
+                for (int i = 0; i < temp.children.size(); i++) {
+                    if (temp.children.get(i) != null) {
+                        queue.offer(temp.children.get(i));
+                    }
+                }
+            }
+        }
+        return depth;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
---
+++ src/indi/alain/Leetcode/test/BinaryTree01.java
@@ -0,0 +1,17 @@
+package indi.alain.Leetcode.test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author d
+ */
+public class BinaryTree01 {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+    }
+}

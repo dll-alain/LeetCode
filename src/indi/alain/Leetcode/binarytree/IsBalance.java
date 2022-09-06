b1e54ea3acbe899f2da7f99991a11355558d4f9b
Alain <18817636063@163.com>
2022-09-06 10:06:26 +0800

    0906

---
+++ src/indi/alain/Leetcode/binarytree/IsBalance.java
@@ -0,0 +1,34 @@
+package indi.alain.Leetcode.binarytree;
+
+/**
+ * @author d
+ */
+public class IsBalance {
+
+    public boolean isBalance(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = getHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
+
+
+
+

09c8d328d367ab1f0bd2bfff42170d004e2a515a
Alain <18817636063@163.com>
2022-09-08 23:31:06 +0800

    0908

---
+++ src/indi/alain/Leetcode/binarytree/SunOfLeftNode.java
@@ -0,0 +1,20 @@
+package indi.alain.Leetcode.binarytree;
+
+/**
+ * @author d
+ */
+public class SunOfLeftNode {
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int leftValue = sumOfLeftLeaves(root.left);    // 左
+        int rightValue = sumOfLeftLeaves(root.right);  // 右
+
+        int midValue = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            midValue = root.left.val;
+        }
+        int sum = midValue + leftValue + rightValue;  // 中
+        return sum;
+    }
+}

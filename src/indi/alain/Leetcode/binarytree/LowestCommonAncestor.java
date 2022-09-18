e168797ff635ec0cfd8ac4065555869dd6b28566
Alain <18817636063@163.com>
2022-09-18 13:45:27 +0800

    0918

---
+++ src/indi/alain/Leetcode/binarytree/LowestCommonAncestor.java
@@ -0,0 +1,23 @@
+package indi.alain.Leetcode.binarytree;
+
+/**
+ * @author d
+ */
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode leftTra = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightTra = lowestCommonAncestor(root.right, p, q);
+        if (leftTra != null && rightTra != null) {
+            return root;
+        } else if (leftTra != null && rightTra == null) {
+            return leftTra;
+        } else if (leftTra == null && rightTra != null) {
+            return rightTra;
+        }
+        return null;
+    }
+}

51167c3c03971108ff180882903e98ae2bfe5f2b
Alain <18817636063@163.com>
2022-10-03 11:31:43 +0800

    1003

--- src/indi/alain/Leetcode/backtracking/CombinationSum3.java
+++ src/indi/alain/Leetcode/backtracking/CombinationSum3.java
@@ -5,37 +5,29 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class CombinationSum {
-    private List<List<Integer>> result = new ArrayList<>();
-    private LinkedList<Integer> path = new LinkedList<>();
+public class CombinationSum3 {
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         backTracking(candidates, target, 0, 0);
         return result;
     }
 
     private void backTracking(int[] candidates, int target, int sum, int startIndex) {
-        // if (sum > target) {
-        //     return;
-        // }
         if (sum == target) {
             result.add(new ArrayList(path));
             return;
         }
-        for (int i = startIndex; i < candidates.length; i++) {
+        for (int i = startIndex; i < candidates.length && sum + candidates[i] <= target; i++) {
+            if (i > startIndex && candidates[i] == candidates[i - 1]) continue;
             sum += candidates[i];
-            if (sum > target) {
-                break;
-            }
             path.add(candidates[i]);
-            backTracking(candidates, target, sum, i);
+            backTracking(candidates, target, sum, i + 1);
             sum -= candidates[i];
             path.removeLast();
         }
     }
-
-
-
 
 }

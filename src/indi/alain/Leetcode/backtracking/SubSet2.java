d5186af47fc6500c0eacd038f526f8cf1a52fe11
Alain <18817636063@163.com>
2022-10-11 22:13:26 +0800

    1011

---
+++ src/indi/alain/Leetcode/backtracking/SubSet2.java
@@ -0,0 +1,37 @@
+package indi.alain.Leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author d
+ */
+public class SubSet2 {
+    private List<List<Integer>> result = new ArrayList<>();
+    private LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        backTracking(nums, 0);
+        return result;
+
+    }
+
+    private void backTracking(int[] nums, int startIndex) {
+        result.add(new ArrayList<>(path));
+        if (startIndex >= nums.length) {
+            return;
+        }
+        for (int i = startIndex; i < nums.length; i++) {
+            if (i > startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            backTracking(nums, i + 1);
+            path.removeLast();
+        }
+    }
+
+}

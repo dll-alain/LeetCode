1638b0740e97c15e4204247c7aa08d9609f93c98
Alain <18817636063@163.com>
2022-11-01 13:41:44 +0800

    1101

---
+++ src/indi/alain/leetcode/greedy_algorithm/AddGasoline.java
@@ -0,0 +1,22 @@
+package indi.alain.leetcode.greedy_algorithm;
+
+/**
+ * @author d
+ */
+public class AddGasoline {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int curSum = 0;
+        int totalSum = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+            if (curSum < 0) {
+                start = i + 1;
+                curSum = 0;
+            }
+        }
+        if (totalSum < 0) return -1;
+        return start;
+    }
+}
